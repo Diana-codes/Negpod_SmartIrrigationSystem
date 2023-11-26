6e8424ddcec71ade944e8e0bbeab495b146b62cc
Diana-codes <dianaruzindana8@gmail.com>
2023-11-26 10:54:45 -0800

    edits in the print statementys

--- introduction_and_guidelines/introduction_and_guidelines.py
+++ introduction_and_guidelines/introduction_and_guidelines.py
@@ -3,6 +3,6 @@ def introduction_and_guidelines():
     print("1.Welcome to the Plant Irrigation System!")
     print("2.This application helps you manage plant irrigation.")
     print("3.Please follow the guidelines for effective plant care.")
-    print("Ensure correct input format and adhere to predefined parameters.")
-    print("If you are a first time user go to the sign up button and if you are a returning user go to the login button")
+    print("4.Ensure correct input format and adhere to predefined parameters.")
+    print("5.If you are a first time user go to the sign up button and if you are a returning user go to the login button")
 introduction_and_guidelines()
